8e41845e42ce2bc6b03d8017e0e5a1879f91f15b
Maicão <maicongoetens@gmail.com>
2025-02-18 21:09:51 -0300

    FooterBar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/FooterBar.vue';
 </script>
 
 <template>
-  <NavBar/>
+
+  <NavBar />
+
   <code>src/views/HomeView.vue</code>
+
+  <Footer />
+
 </template>
