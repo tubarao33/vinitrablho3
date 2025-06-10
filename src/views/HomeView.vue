4c249036bd2fb107399b385fb12ca18447ecd9e5
tubarao33 <guerraviniciusguerra870@gmail.com>
2025-06-10 16:45:49 -0300

    Update HomeView.vue
    comentei linha 6 para retirada bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,6 @@ import Footer from '@/components/FooterBar.vue';
 </script>
 
 <template>
-  <code>src/views/HomeView.vue</code>
+  <!--<code>src/views/HomeView.vue</code>-->
   <Footer />
 </template>
